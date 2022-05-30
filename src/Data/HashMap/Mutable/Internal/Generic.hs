12aa291a4fbc5309cc62b279603ecdf41c830b27
Brian Shu <littlebubu.shu@gmail.com>
2022-05-30 09:52:07 -0400

    .

--- src/Data/HashMap/Mutable/Internal/Generic.hs
+++ src/Data/HashMap/Mutable/Internal/Generic.hs
@@ -7,14 +7,16 @@ import Control.Monad.Primitive
 import Data.Bits ((.&.))
 import Data.Functor ((<&>))
 -- import Data.HashMap.Mutable.Internal.Array qualified as Array
+
+import Data.HashMap.Mutable.Internal.Primes qualified as Primes
 import Data.Hashable (Hashable)
 import Data.Hashable qualified as Hashable
-import Data.Primitive.Contiguous qualified as Array
-import Data.Primitive.MutVar
-import Data.Primitive.PrimArray (PrimArray, primArrayFromList, MutablePrimArray, sizeofMutablePrimArray)
-import GHC.Exts qualified as Exts
 import Data.Primitive (Prim)
-import qualified Data.Primitive.Contiguous.Class as Array
+import Data.Primitive.Contiguous qualified as Array
+import Data.Primitive.Contiguous.Class qualified as Array
+import Data.Primitive.MutVar
+import Data.Primitive.PrimArray (MutablePrimArray, sizeofMutablePrimArray)
+import GHC.Exts qualified as Exts
 
 type role HashMap nominal nominal nominal nominal
 
@@ -23,6 +25,7 @@ newtype HashMap arr s k v = HashMap {var :: MutVar s (HashMap_ arr s k v)}
 data HashMap_ arr s k v = -- | Invariants: buckets, hashes, links, keys, and values must all be the same size
   -- A value with the same index in hashes, links, and keys forms an entry. The index is called the entryIndex.
   -- Entries are only valid from indices 0..size, exclusive on size
+  -- TODO: use one array for the control stuff
   HashMap_
   { -- | Invariant: Must be of length 3
     refs :: !(MutablePrimArray s Int),
@@ -60,7 +63,7 @@ newWithCapacity capacity = newWithCapacity_ capacity >>= newMutVar <&> HashMap
 
 newWithCapacity_ :: (HasArray arr k v, PrimMonad m) => Int -> m (HashMap_ arr (PrimState m) k v)
 newWithCapacity_ capacity = do
-  let capacity' = getPrime capacity
+  let capacity' = Primes.getPrime capacity
   refs <- Array.replicateMut 3 0
   Array.write refs freeListRef -1
   buckets <- Array.replicateMut capacity' -1
@@ -222,7 +225,7 @@ insert_' hash key value HashMap {var} map@HashMap_ {refs, buckets, hashes, links
             Array.write refs sizeRef $! size + 1
             if size == bucketsSize buckets
               then do
-                let newSize = getPrime $ size * 2
+                let newSize = Primes.getPrime $ size * 2
                 map' <- resize_ newSize map
                 add' size (hashCode `rem` newSize) map'
                 writeMutVar var map'
@@ -280,38 +283,9 @@ resize_ newSize map@HashMap_ {buckets, hashes, links, keys, values} = do
       }
 {-# INLINE resize_ #-}
 
-getPrime :: Int -> Int
-getPrime n
-  | n >= maxPrime = n
-  | otherwise = go 0
-  where
-    go !i = do
-      let !p = Array.index primes i
-      if p >= n
-        then p
-        else go $ i + 1
-{-# INLINE getPrime #-}
-
-maxPrime :: Int
-maxPrime = Array.index primes $ Array.size primes - 1
-
 bucketsSize :: Prim a => MutablePrimArray s a -> Int
 bucketsSize = sizeofMutablePrimArray
 {-# INLINE bucketsSize #-}
-
-{- ORMOLU_DISABLE -}
-primes :: PrimArray Int
-primes = primArrayFromList [
-  7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631,
-  761, 919, 1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143,
-  14591, 17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363,
-  156437, 187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897,
-  1162687, 1395263, 1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471,
-  7199369, 8639249, 10367101, 12440537, 14928671, 17914409, 21497293, 25796759, 30956117,
-  37147349, 44576837, 53492207, 64190669, 77028803, 92434613, 110921543, 133105859, 159727031,
-  191672443, 230006941, 276008387, 331210079, 397452101, 476942527, 572331049, 686797261,
-  824156741, 988988137, 1186785773, 1424142949, 1708971541, 2050765853 ]
-{- ORMOLU_ENABLE -}
 
 undefinedElem :: forall a. a
 undefinedElem = error "Undefined element"
