9cf6b0c0c4279d51dbb4d462718ce889fa988bdd
brian <littlebubu.shu@gmail.com>
2022-07-03 15:38:23 -0400

    fix benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -3,36 +3,39 @@
 
 module Main where
 
+import Control.Exception (evaluate)
+import Control.Monad (void)
 import Control.Monad.Primitive (PrimMonad, PrimState)
 import Control.Monad.Random (MonadRandom)
 import Control.Monad.Random.Strict qualified as Monad.Random
 import Criterion.Main
-import Data.Constraint (Dict (..))
-import Data.Constraint qualified as Constraint
 import Data.Foldable qualified as Foldable
+import Data.Functor ((<&>))
 import Data.HashMap.Mutable qualified as HashMap.Mutable.Arena
 import Data.HashMap.Mutable.Internal.Robin qualified as HashMap.Mutable.Robin
-import Data.HashTable.IO qualified as HashTable
-import Data.Kind (Type)
 import Data.Primitive (Array)
-import Data.Proxy
 import Data.Vector qualified as VB
 import Data.Vector.Hashtables qualified as Vector.Hashtables
 import Data.Vector.Hashtables.Internal qualified as VH
 import Data.Vector.Mutable qualified as VBM
-import GHC.Base (RealWorld)
 import Immutable.Shuffle qualified
-import IsMap (IsMap)
-import IsMap qualified
+import MapFunctions (MapFunctions (..))
+import MapFunctions qualified hiding (lookup)
 import System.Random (Random)
+import System.Random qualified as Random
+import Prelude hiding (lookup)
 
-insertMonotonic :: Int -> (Int -> IO h) -> (Int -> Int -> h -> IO ()) -> IO h
-insertMonotonic n new insert = do
+insertMonotonic :: Int -> (Int -> IO h) -> (Int -> Int -> h -> IO ()) -> (Int -> h -> IO (Maybe a)) -> IO h
+insertMonotonic n new insert lookup = do
   map <- new n
-  let go i
-        | i <= n = do insert i i map; go $ i + 1
+  let go1 i
+        | i <= n = do insert i i map; go1 $ i + 1
         | otherwise = pure ()
-  go 0
+  go1 0
+  let go2 i
+        | i <= n = do lookup i map; go2 $ i + 2
+        | otherwise = pure ()
+  go2 0
   pure map
 {-# NOINLINE insertMonotonic #-}
 
@@ -47,119 +50,152 @@ vhfind n ht = do
 
 main :: IO ()
 main = do
-  let inputs = [100, 1000, 10000, 100000, 1000000]
-  comparisonBench <- mapM comparisonBenches inputs
-  defaultMain
-    [ bgroup "comparison" comparisonBench
+  let inputs = [100, 1000, 10000, 100000]
+  defaultMain $ fmap makeBenches inputs
+
+makeBenches :: Int -> Benchmark
+makeBenches n =
+  bgroup (show n) [makeIntBenches n]
+
+makeBenches' :: Int -> Benchmark
+makeBenches' n =
+  bgroup
+    (show n)
+    [ bgroup
+        "insert"
+        [ bench "mutable-hashmaps arena" $
+            whnfIO $
+              insertMonotonic
+                n
+                (HashMap.Mutable.Arena.newWithCapacity)
+                HashMap.Mutable.Arena.insert
+                HashMap.Mutable.Arena.lookup,
+          bench "mutable-hashmaps robin" $
+            whnfIO $
+              insertMonotonic
+                n
+                (HashMap.Mutable.Robin.newWithCapacity @Array)
+                HashMap.Mutable.Robin.insert
+                HashMap.Mutable.Robin.lookup,
+          bench "vector-hashtables" $
+            whnfIO $
+              insertMonotonic
+                n
+                (Vector.Hashtables.initialize @VBM.MVector @Int @VBM.MVector @Int)
+                (\i j m -> Vector.Hashtables.insert m i j)
+                (\i m -> Vector.Hashtables.lookup m i)
+        ]
+        -- bgroup
+        --   "insert resize"
+        --   [ bench "mutable-hashmaps arena" $
+        --       whnfIO $
+        --         insertMonotonic
+        --           n
+        --           (const (HashMap.Mutable.Arena.new))
+        --           HashMap.Mutable.Arena.insert,
+        --     bench "mutable-hashmaps robin" $
+        --       whnfIO $
+        --         insertMonotonic
+        --           n
+        --           (const (HashMap.Mutable.Robin.new @Array))
+        --           HashMap.Mutable.Robin.insert,
+        --     bench "vector-hashtables" $
+        --       whnfIO $
+        --         insertMonotonic
+        --           n
+        --           (const $ Vector.Hashtables.initialize @VBM.MVector @Int @VBM.MVector @Int 10)
+        --           (\i j m -> Vector.Hashtables.insert m i j),
+        --     bench "hashtables basic" $
+        --       whnfIO $
+        --         insertMonotonic
+        --           n
+        --           (const (HashTable.new :: IO (HashTable.BasicHashTable Int Int)))
+        --           (\i j m -> HashTable.insert m i j)
+        --   ]
     ]
 
-comparisonBenches :: Int -> IO Benchmark
-comparisonBenches n =
-  pure $
-    bgroup
-      (show n)
-      [ bgroup
-          "insert"
-          [ bench "mutable-hashmaps arena" $
-              whnfIO $
-                insertMonotonic
-                  n
-                  (HashMap.Mutable.Arena.newWithCapacity)
-                  HashMap.Mutable.Arena.insert,
-            bench "mutable-hashmaps robin" $
-              whnfIO $
-                insertMonotonic
-                  n
-                  (HashMap.Mutable.Robin.newWithCapacity @Array)
-                  HashMap.Mutable.Robin.insert,
-            bench "vector-hashtables" $
-              whnfIO $
-                insertMonotonic
-                  n
-                  (Vector.Hashtables.initialize @VBM.MVector @Int @VBM.MVector @Int)
-                  (\i j m -> Vector.Hashtables.insert m i j)
-          ],
-        bgroup
-          "insert resize"
-          [ bench "mutable-hashmaps arena" $
-              whnfIO $
-                insertMonotonic
-                  n
-                  (const (HashMap.Mutable.Arena.new))
-                  HashMap.Mutable.Arena.insert,
-            bench "mutable-hashmaps robin" $
-              whnfIO $
-                insertMonotonic
-                  n
-                  (const (HashMap.Mutable.Robin.new @Array))
-                  HashMap.Mutable.Robin.insert,
-            bench "vector-hashtables" $
-              whnfIO $
-                insertMonotonic
-                  n
-                  (const $ Vector.Hashtables.initialize @VBM.MVector @Int @VBM.MVector @Int 10)
-                  (\i j m -> Vector.Hashtables.insert m i j),
-            bench "hashtables basic" $
-              whnfIO $
-                insertMonotonic
-                  n
-                  (const (HashTable.new :: IO (HashTable.BasicHashTable Int Int)))
-                  (\i j m -> HashTable.insert m i j)
-          ]
-      ]
+makeIntBenches :: Int -> Benchmark
+makeIntBenches n =
+  env
+    ( do
+        stdGen <- Random.newStdGen
+        flip Monad.Random.evalRandT stdGen $ do
+          fullInts <- getFullInts n (0, n - 1)
+          fullIntsShuffled <- Immutable.Shuffle.shuffleM fullInts
+          rangeInts <- getShuffledRangeInts (0, n - 1)
+          pure (stdGen, fullInts, fullIntsShuffled, rangeInts)
+    )
+    withEnv
+  where
+    withEnv ~(stdGen, fullInts, fullIntsShuffled, rangeInts) =
+      bgroup
+        "int"
+        [ lookupFullBenches,
+          lookupMissBenches,
+          insertFullBenches,
+          insertRangeBenches
+        ]
+      where
+        allBenches =
+          bgroup "all" $
+            MapFunctions.allMapFunctions
+              <&> \fs -> bench (name fs) (whnfIO (benchAll fs fullInts))
+        insertFullBenches =
+          bgroup "insertFull" $
+            MapFunctions.allMapFunctions
+              <&> \fs -> bench (name fs) (whnfIO (benchInsert fs fullInts))
+        insertRangeBenches =
+          bgroup "insertRange" $
+            MapFunctions.allMapFunctions
+              <&> \fs -> bench (name fs) (whnfIO $ benchInsert fs rangeInts)
+        lookupFullBenches =
+          bgroup "lookupFull" $
+            MapFunctions.allMapFunctions
+              <&> \fs -> bench (name fs) (whnfIO $ benchLookup fs fullInts fullIntsShuffled)
+        lookupMissBenches =
+          env
+            ( flip Monad.Random.evalRandT stdGen $ do
+                is <- getFullInts n (0, maxBound)
+                is' <- getFullInts n (minBound, -3)
+                pure (is, is')
+            )
+            withEnv
+          where
+            withEnv ~(is, is') =
+              bgroup "lookupMiss" $
+                MapFunctions.allMapFunctions
+                  <&> \fs -> bench (name fs) (whnfIO $ benchLookup fs is is')
 
-makeBenches :: forall map. Proxy map -> Int -> Benchmark
-makeBenches _ n =
-  bgroup
-    []
-    undefined
+-- benches =
 
-data IsMapDict :: Type -> Type -> Type where
-  IsMapDict :: (Dict (IsMap map, IsMap.Key map ~ k, IsMap.Value map ~ v)) -> IsMapDict k v
-
-withIsMapDict :: IsMapDict -> (forall map. IsMap map => r) -> r
-withIsMapDict con r = case con of
-  (IsMapDict (Dict :: Dict (IsMap map))) -> r @map
-
-isMapDict :: forall map. IsMap map => IsMapDict
-isMapDict = IsMapDict (Dict @(IsMap map))
-
--- mapConstraints :: [forall map. Dict (IsMap map)]
--- mapDicts :: [IsMapDict]
-mapDicts =
-  [ isMapDict @(HashMap.Mutable.Arena.HashMap RealWorld Int Int)
-  ]
-
-getRandomVec :: (MonadRandom m, Random a) => m (VB.Vector a)
+getRandomVec :: (Random a, MonadRandom m) => m (VB.Vector a)
 getRandomVec = VB.fromList <$> Monad.Random.getRandoms
 
-getRandomVecRs :: (MonadRandom m, Random a) => (a, a) -> m (VB.Vector a)
+getRandomVecRs :: (Random a, MonadRandom m) => (a, a) -> m (VB.Vector a)
 getRandomVecRs rs = VB.fromList <$> Monad.Random.getRandomRs rs
 
-getFullInts :: MonadRandom m => Int -> m (VB.Vector Int)
-getFullInts n = VB.generateM n $ \_ -> Monad.Random.getRandom
+getFullInts :: MonadRandom m => Int -> (Int, Int) -> m (VB.Vector Int)
+getFullInts n r = VB.generateM n $ \_ -> Monad.Random.getRandomR r
 
-getShuffledRangeInts :: (MonadRandom m, PrimMonad m) => Int -> m (VB.Vector Int)
-getShuffledRangeInts n = Immutable.Shuffle.shuffleM $ VB.enumFromTo 0 (n - 1)
+getShuffledRangeInts :: (MonadRandom m, PrimMonad m) => (Int, Int) -> m (VB.Vector Int)
+getShuffledRangeInts (i, j) = Immutable.Shuffle.shuffleM $ VB.enumFromTo i j
 
-type IsSimpleMap map k = (IsMap map, IsMap.Key map ~ k, IsMap.Value map ~ k)
+benchInsert :: MapFunctions k k -> VB.Vector k -> IO ()
+benchInsert MapFunctions {new, insert} ks = do
+  m <- new
+  void $ evaluate $ Foldable.foldlM (\m k -> insert k k $! m) m ks
 
-benchInsert :: forall map k. IsSimpleMap map k => Proxy map -> VB.Vector k -> IO map
-benchInsert _ ks = do
-  m <- IsMap.new @map
-  Foldable.foldlM (\m k -> IsMap.insert k k $! m) m ks
+benchAll :: MapFunctions k k -> VB.Vector k -> IO ()
+benchAll MapFunctions {new, insert, delete, lookup} ks = do
+  m <- new
+  m <- Foldable.foldlM (\m k -> insert k k $! m) m ks
+  m <- Foldable.foldlM (\m k -> delete k m) m $ VB.take (VB.length ks `div` 2) ks
+  VB.forM_ ks $ \k -> lookup k m
+  void $ evaluate m
 
-benchAll :: forall map k. IsSimpleMap map k => Proxy map -> VB.Vector k -> IO map
-benchAll _ ks = do
-  m <- IsMap.new @map
-  m <- Foldable.foldlM (\m k -> IsMap.insert k k $! m) m ks
-  m <- Foldable.foldlM (\m k -> IsMap.delete k m) m $ VB.take (VB.length ks `div` 2) ks
-  VB.forM_ ks $ \k -> IsMap.lookup k m
-  pure m
-
-benchLookup :: forall map k. (IsMap map, IsMap.Key map ~ k, IsMap.Value map ~ k) => Proxy map -> VB.Vector k -> VB.Vector k -> IO map
-benchLookup _ ks ks' = do
-  m <- IsMap.new @map
-  m <- Foldable.foldlM (\m k -> IsMap.insert k k $! m) m ks
-  VB.forM_ ks' $ \k' -> IsMap.lookup k' m
-  pure m
+benchLookup :: MapFunctions k k -> VB.Vector k -> VB.Vector k -> IO ()
+benchLookup MapFunctions {new, insert, lookup} ks ks' = do
+  m <- new
+  m <- Foldable.foldlM (\m k -> insert k k $! m) m ks
+  VB.forM_ ks' $ \k' -> lookup k' m
+  void $ evaluate m
---
+++ bench/MapFunctions.hs
@@ -0,0 +1,73 @@
+module MapFunctions where
+
+import Data.Functor (($>))
+import Data.HashMap.Mutable qualified as Arena
+import Data.HashMap.Mutable.Internal.Robin qualified as Robin
+import Data.HashMap.Strict qualified as Data.HashMap
+import Data.Hashable (Hashable)
+import Data.Primitive.Array (Array)
+import Data.Vector.Hashtables qualified as Vector.Hashtables
+import Data.Vector.Mutable qualified as VBM
+
+data MapFunctions k v = forall map.
+  MapFunctions
+  { name :: String,
+    new :: IO map,
+    insert :: k -> v -> map -> IO map,
+    delete :: k -> map -> IO map,
+    lookup :: k -> map -> IO (Maybe v)
+  }
+
+arenaFunctions :: Hashable k => MapFunctions k v
+arenaFunctions =
+  MapFunctions
+    { name = "mutable-hashmap arena",
+      new = Arena.new,
+      insert = \key value map -> Arena.insert key value map $> map,
+      delete = \key map -> Arena.delete key map $> map,
+      lookup = \key map -> Arena.lookup key map
+    }
+{-# SPECIALIZE arenaFunctions :: MapFunctions Int Int #-}
+
+robinFunctions :: Hashable k => MapFunctions k v
+robinFunctions =
+  MapFunctions
+    { name = "mutable-hashmap robin",
+      new = Robin.new @Array,
+      insert = \key value map -> Robin.insert key value map $> map,
+      -- delete = \key value map -> Robin.delete key value map $> map,
+      delete = undefined,
+      lookup = \key map -> Robin.lookup key map
+    }
+{-# SPECIALIZE robinFunctions :: MapFunctions Int Int #-}
+
+vectorFunctions :: forall k v. Hashable k => MapFunctions k v
+vectorFunctions =
+  MapFunctions
+    { name = "vector-hashtables",
+      new = Vector.Hashtables.initialize @VBM.MVector @k @VBM.MVector @v 10,
+      insert = \key value map -> Vector.Hashtables.insert map key value $> map,
+      lookup = \key map -> Vector.Hashtables.lookup map key,
+      delete = \key map -> Vector.Hashtables.delete map key $> map
+    }
+{-# SPECIALIZE vectorFunctions :: MapFunctions Int Int #-}
+
+unorderedContainersFunctions :: Hashable k => MapFunctions k v
+unorderedContainersFunctions =
+  MapFunctions
+    { name = "unordered-containers",
+      new = pure mempty,
+      insert = \key value map -> pure $! Data.HashMap.insert key value map,
+      lookup = \key map -> pure $! Data.HashMap.lookup key map,
+      delete = \key map -> pure $! Data.HashMap.delete key map
+    }
+{-# SPECIALIZE unorderedContainersFunctions :: MapFunctions Int Int #-}
+
+allMapFunctions :: Hashable k => [MapFunctions k v]
+allMapFunctions =
+  [ arenaFunctions,
+    robinFunctions,
+    vectorFunctions,
+    unorderedContainersFunctions
+  ]
+{-# SPECIALIZE allMapFunctions :: [MapFunctions Int Int] #-}
